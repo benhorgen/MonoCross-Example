77511d17b0979c332bad87432e6f6f414c1d3dc5
Ben Horgen <ben@benhorgen.com>
2012-05-24 11:23:02 -0500

    Revert "Cleaned up button color in iOS and Touch views"
    This reverts commit 6c2cb84197316dae8408a00440e6e13e76d16d20.

--- ExampleApp.Droid/Views/WelcomeScreenView.cs
+++ ExampleApp.Droid/Views/WelcomeScreenView.cs
@@ -16,14 +16,9 @@ namespace ExampleApp.Droid
 			SetContentView(Resource.Layout.WelcomeView);
 			
 			Button b = (Button)FindViewById(Resource.Id.myButton);
-			if (b != null) 
-			{
-				Android.Graphics.Color color = Android.Graphics.Color.Rgb(0x62, 0x63, 0x70);
-				Android.Graphics.PorterDuff.Mode mode = Android.Graphics.PorterDuff.Mode.Multiply;
-				
-				var c = new Android.Graphics.PorterDuffColorFilter(color, mode);
-				b.Background.SetColorFilter(c);
-				b.Click += (sender, e) => { MXDroidContainer.Navigate("Dashboard");  }; 
+			if (b != null) { b.Click += (sender, e) => 
+				{ 
+					MXDroidContainer.Navigate("Dashboard");  }; 
 			}
         }
     }
